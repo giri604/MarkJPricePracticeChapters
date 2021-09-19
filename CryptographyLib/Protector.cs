0df05758e43b443f563038febd348ada62dedd26
giri604 <52734895+giri604@users.noreply.github.com>
2021-09-19 10:13:23 +0530

    Chapter 10 Signing with SHA256 and RSA

--- CryptographyLib/Protector.cs
+++ CryptographyLib/Protector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Packt.Shared
 {
@@ -93,6 +94,76 @@ namespace Packt.Shared
             var sha = SHA256.Create();
             var saltedPassword = password + salt;
             return Convert.ToBase64String(sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+        }
+
+        public static string PublicKey;
+
+        public static string ToXmlStringExt(this RSA rsa, bool includePrivateParameters)
+        {
+            var p = rsa.ExportParameters(includePrivateParameters);
+            XElement xml;
+            if (includePrivateParameters)
+            {
+                xml = new XElement("RSAKeyValue",
+                    new XElement("Modulus", Convert.ToBase64String(p.Modulus)),
+                    new XElement("Exponent", Convert.ToBase64String(p.Exponent)),
+                    new XElement("P", Convert.ToBase64String(p.P)),
+                    new XElement("Q", Convert.ToBase64String(p.Q)),
+                    new XElement("DP", Convert.ToBase64String(p.DP)),
+                    new XElement("DQ", Convert.ToBase64String(p.DQ)),
+                    new XElement("InverseQ", Convert.ToBase64String(p.InverseQ)));
+            }
+            else
+            {
+                xml = new XElement("RSAKeyValue",
+                        new XElement("Modulus", Convert.ToBase64String(p.Modulus)),
+                        new XElement("Exponent", Convert.ToBase64String(p.Exponent)));
+            }
+            return xml?.ToString();
+        }
+
+        public static void FromXMlStringExt(this RSA rsa, string parameterAsXml)
+        {
+            var xml = XDocument.Parse(parameterAsXml);
+            var root = xml.Element("RSAKeyValue");
+            var p = new RSAParameters()
+            {
+                Modulus = Convert.FromBase64String(root.Element("Modulus").Value),
+                Exponent = Convert.FromBase64String(root.Element("Exponent").Value)
+            };
+
+            if (root.Element("p") != null)
+            {
+                p.P = Convert.FromBase64String(root.Element("P").Value);
+                p.Q = Convert.FromBase64String(root.Element("Q").Value);
+                p.DP = Convert.FromBase64String(root.Element("DP").Value);
+                p.DQ = Convert.FromBase64String(root.Element("DQ").Value);
+                p.InverseQ = Convert.FromBase64String(root.Element("InverseQ").Value);
+            }
+            rsa.ImportParameters(p);
+        }
+
+        public static string GenerateSignature(string data)
+        {
+            byte[] dataBytes = Encoding.Unicode.GetBytes(data);
+            var sha = SHA256.Create();
+            var hashedData = sha.ComputeHash(dataBytes);
+            var rsa = RSA.Create();
+            PublicKey = rsa.ToXmlStringExt(false);
+
+            return Convert.ToBase64String(rsa.SignHash(hashedData, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
+        }
+
+        public static bool ValidateSignature(string data, string signature)
+        {
+            byte[] dataBytes = Encoding.Unicode.GetBytes(data);
+            var sha = SHA256.Create();
+            var hashedData = sha.ComputeHash(dataBytes);
+            byte[] signatureBytes = Convert.FromBase64String(signature);
+            var rsa = RSA.Create();
+            rsa.FromXMlStringExt(PublicKey);
+
+            return rsa.VerifyHash(hashedData, signatureBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
         }
     }
 }
